35e3b876723528d2167b05dda306b1f5ff4ff0b8
kentakom1213 <kentakom1213@gmail.com>
2023-03-30 23:23:49 +0900

    [fix] BIT上の二分探索のバグを修正

--- Rust/src/BIT.rs
+++ Rust/src/BIT.rs
@@ -1,80 +1,123 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
-use num_traits::identities::Zero;
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+/// # Monoid
+pub trait Monoid {
+    type Val: Clone + PartialEq;
+    const E: Self::Val;
+    fn op(left: &Self::Val, right: &Self::Val) -> Self::Val;
+}
+
+pub trait OrderedMonoid: Monoid {
+    fn lt(left: &Self::Val, right: &Self::Val) -> bool;
+}
 
 /// # BinaryIndexedTree
 /// - `0-indexed`なインターフェースを持つBIT
-pub struct BIT<T> {
+pub struct BIT<T: Monoid> {
     pub size: usize,
-    arr: Vec<T>,
+    arr: Vec<T::Val>,
 }
 
-impl<T> BIT<T>
-where
-    T: Copy + Clone + Zero + Add + AddAssign + Sub<Output = T> + SubAssign + PartialOrd,
-{
+impl<T: Monoid> BIT<T> {
+    #[inline]
+    fn lsb(x: usize) -> usize {
+        x & x.wrapping_neg()
+    }
+
     pub fn new(n: usize) -> Self {
         BIT {
             size: n,
-            arr: vec![T::zero(); n + 1],
+            arr: vec![T::E; n + 1],
         }
     }
 
-    pub fn build(src: &[T]) -> Self {
+    pub fn add(&mut self, mut i: usize, x: T::Val) {
+        i += 1;
+        while i <= self.size {
+            self.arr[i] = T::op(&self.arr[i], &x);
+            i += Self::lsb(i);
+        }
+    }
+
+    pub fn prefix_sum(&self, mut i: usize) -> T::Val {
+        let mut res = T::E;
+        while i != 0 {
+            res = T::op(&res, &self.arr[i]);
+            i -= Self::lsb(i);
+        }
+        res
+    }
+}
+
+impl<T: Monoid> From<&Vec<T::Val>> for BIT<T> {
+    fn from(src: &Vec<T::Val>) -> Self {
         let size = src.len();
-        let mut arr = vec![T::zero(); size + 1];
+        let mut arr = vec![T::E; size + 1];
         for i in 1..=size {
-            let x = src[i - 1];
-            arr[i] += x;
-            let tmp = arr[i];
-            let j = i + (i & i.wrapping_neg());
+            let x = src[i - 1].clone();
+            arr[i] = T::op(&arr[i], &x);
+            let j = i + Self::lsb(i);
             if j < size + 1 {
-                arr[j] += tmp;
+                arr[j] = T::op(&arr[j], &arr[i].clone());
             }
         }
         Self { size, arr }
     }
+}
 
-    pub fn add(&mut self, mut i: usize, x: T) {
-        i += 1;
-        while i <= self.size {
-            self.arr[i] += x;
-            i += i & i.wrapping_neg();
-        }
-    }
-
-    pub fn prefix_sum(&self, mut i: usize) -> T {
-        let mut res = T::zero();
-        while i != 0 {
-            res += self.arr[i];
-            i -= i & i.wrapping_neg();
-        }
-        res
-    }
-
-    pub fn sum(&self, l: usize, r: usize) -> T {
-        let to_l = self.prefix_sum(l);
-        let to_r = self.prefix_sum(r);
-        to_r - to_l
-    }
-
-    /// ## lower_bound
-    /// - `0..x`の和が`w`以上になる最小の`x`を求める
-    /// - 0-indexed
-    pub fn lower_bound(&self, w: T) -> usize {
-        let mut sum = T::zero();
+impl<T: OrderedMonoid> BIT<T> {
+    pub fn lower_bound(&self, w: T::Val) -> usize {
+        let mut sum = T::E;
         let mut idx = 0;
-        let depth = self.size.ilog2() as usize;
-        for i in (0..=depth).rev() {
-            let k = idx + (1 << i);
-            if k <= self.size && sum + self.arr[k] < w {
-                sum += self.arr[k];
-                idx += 1 << i;
+        let mut d = self.size.next_power_of_two() / 2;
+        while d != 0 {
+            if idx + d <= self.size {
+                let nxt = T::op(&sum, &self.arr[idx + d]);
+                if T::lt(&nxt, &w) {
+                    sum = nxt;
+                    idx += d;
+                }
             }
+            d >>= 1;
         }
         idx
+    }
+}
+
+mod Alg {
+    use super::{Monoid, OrderedMonoid};
+
+    pub struct Add;
+    impl Monoid for Add {
+        type Val = isize;
+        const E: Self::Val = 0;
+        fn op(left: &Self::Val, right: &Self::Val) -> Self::Val {
+            left + right
+        }
+    }
+    impl OrderedMonoid for Add {
+        fn lt(left: &Self::Val, right: &Self::Val) -> bool {
+            left < right
+        }
+    }
+
+    pub struct Mul;
+    impl Monoid for Mul {
+        type Val = isize;
+        const E: Self::Val = 1;
+        fn op(left: &Self::Val, right: &Self::Val) -> Self::Val {
+            left + right
+        }
+    }
+
+    pub struct Xor;
+    impl Monoid for Xor {
+        type Val = usize;
+        const E: Self::Val = 0;
+        fn op(left: &Self::Val, right: &Self::Val) -> Self::Val {
+            left ^ right
+        }
     }
 }
 
@@ -84,7 +127,7 @@ mod test {
 
     #[test]
     fn test_new() {
-        let mut bit = BIT::<isize>::new(5);
+        let mut bit = BIT::<Alg::Add>::new(5);
 
         bit.add(0, 20);
         bit.add(2, -5);
@@ -104,9 +147,9 @@ mod test {
 
     #[test]
     fn test_build() {
-        let mut bit = BIT::build(&vec![1, 2, 3, 4, 5]);
+        let mut bit = BIT::<Alg::Add>::from(&vec![1, 2, 3, 4, 5]);
 
-        assert_eq!(bit.sum(1, 4), 9);
+        assert_eq!(bit.prefix_sum(4), 10);
         assert_eq!(bit.prefix_sum(5), 15);
 
         bit.add(2, -3);
@@ -117,7 +160,7 @@ mod test {
 
     #[test]
     fn test_lowerbound() {
-        let bit = BIT::build(&vec![1, 2, 3, 4, 5]);
+        let bit = BIT::<Alg::Add>::from(&vec![1, 2, 3, 4, 5]);
 
         assert_eq!(bit.lower_bound(0), 0);
         assert_eq!(bit.lower_bound(1), 0);
