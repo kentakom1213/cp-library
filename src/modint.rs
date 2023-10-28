cf043974f3129b0c00385b50ff0cf20962e4133f
kentakom1213 <kentakom1213@gmail.com>
2023-10-29 01:39:16 +0900

    [add] modint（struct版）を追加

--- src/modint.rs
+++ src/modint.rs
@@ -2,7 +2,119 @@
 
 #![allow(dead_code)]
 
-#[derive()]
-pub struct Modint {
+#[rustfmt::skip] use std::{fmt::Display,ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub}};
+#[rustfmt::skip] #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)] pub struct Modint<const MOD: usize>(usize);
+#[rustfmt::skip] impl<const MOD: usize> Modint<MOD> { fn new(n: usize) -> Self { Self(if n < MOD { n } else { n % MOD }) } }
+#[rustfmt::skip] impl<const MOD: usize> Neg for Modint<MOD> { type Output = Self; fn neg(self) -> Self { Modint(if self.0 == 0 { 0 } else { MOD - self.0 }) } }
+#[rustfmt::skip] impl<const MOD: usize> Add for Modint<MOD> { type Output = Self; fn add(self, rhs: Self) -> Self { let mut res = self.0 + rhs.0; if res > MOD { res -= MOD; } Modint(res) } }
+#[rustfmt::skip] impl<const MOD: usize> Sub for Modint<MOD> { type Output = Self; fn sub(self, rhs: Self) -> Self { self + (- rhs) } }
+#[rustfmt::skip] impl<const MOD: usize> Mul for Modint<MOD> { type Output = Self; fn mul(self, rhs: Self) -> Self { Modint(self.0 * rhs.0 % MOD) } }
+#[rustfmt::skip] impl<const MOD: usize> Div for Modint<MOD> { type Output = Self; fn div(self, rhs: Self) -> Self { self * rhs.inv() } }
+#[rustfmt::skip] impl<const MOD: usize> AddAssign for Modint<MOD> { fn add_assign(&mut self, rhs: Self) { self.0 = (*self + rhs).0 } }
+#[rustfmt::skip] impl<const MOD: usize> MulAssign for Modint<MOD> { fn mul_assign(&mut self, rhs: Self) { self.0 = (*self * rhs).0 } }
+#[rustfmt::skip] impl<const MOD: usize> From<usize> for Modint<MOD> { fn from(value: usize) -> Self { Modint::new(value) } }
+#[rustfmt::skip] impl<const MOD: usize> Add<usize> for Modint<MOD> { type Output = Self; fn add(self, rhs: usize) -> Self { let mut res = self.0 + rhs; if res > MOD {res -= MOD;} Modint(res) } }
+#[rustfmt::skip] impl<const MOD: usize> Sub<usize> for Modint<MOD> { type Output = Self; fn sub(self, rhs: usize) -> Self { self - Modint::new(rhs) } }
+#[rustfmt::skip] impl<const MOD: usize> Mul<usize> for Modint<MOD> { type Output = Self; fn mul(self, rhs: usize) -> Self { self * Modint::new(rhs) } }
+#[rustfmt::skip] impl<const MOD: usize> Div<usize> for Modint<MOD> { type Output = Self; fn div(self, rhs: usize) -> Self { self / Modint::new(rhs) } }
+#[rustfmt::skip] impl<const MOD: usize> AddAssign<usize> for Modint<MOD> { fn add_assign(&mut self, rhs: usize) { *self += Modint::new(rhs) } }
+#[rustfmt::skip] impl<const MOD: usize> MulAssign<usize> for Modint<MOD> { fn mul_assign(&mut self, rhs: usize) { *self *= Modint::new(rhs) } }
+#[rustfmt::skip] impl<const MOD: usize> Display for Modint<MOD> { fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result { write!(f, "{}", self.0) } }
+#[rustfmt::skip] impl<const MOD: usize> PartialEq<usize> for Modint<MOD> { fn eq(&self, other: &usize) -> bool { self == &Modint::new(*other) } }
+#[rustfmt::skip] pub trait Fp { fn pow(&self, rhs: usize) -> Self; fn inv(&self) -> Self; }
+#[rustfmt::skip] impl<const MOD: usize> Fp for Modint<MOD> { fn pow(&self, rhs: usize) -> Self { let (mut a, mut b) = (self.0, rhs); let mut res = 1; while b > 0 { if b & 1 == 1 { res = (res * a) % MOD; } a = (a * a) % MOD; b >>= 1; } Modint(res) } fn inv(&self) -> Self { self.pow(MOD - 2) } }
 
+#[cfg(test)]
+mod test {
+    use crate::modint::Fp;
+
+    use super::Modint;
+
+    const MOD998: usize = 998244353;
+    type Modint998 = Modint<MOD998>;
+
+    #[test]
+    fn test_add() {
+        let x: Modint998 = 998244355.into();
+        let y: usize = 998244359;
+        assert_eq!(x + y, 8 + MOD998);
+
+        let a: Modint998 = MOD998.into();
+        let b = 1000000007;
+        let c = 20021213;
+        assert_eq!(a + b + c, 21776867);
+        assert_eq!(a + b + c, (21776867 + MOD998));
+    }
+
+    #[test]
+    fn test_neg() {
+        let x: Modint998 = 0.into();
+        assert_eq!(-x, 0);
+
+        let y = Modint998::new(10);
+        assert_eq!(-y, MOD998 - 10);
+
+        let z = Modint998::new(MOD998 + 200);
+        assert_eq!(-z, MOD998 - 200);
+    }
+
+    #[test]
+    fn test_sub() {
+        let x = Modint998::new(0);
+        let y = 1000000007;
+        assert_eq!(x - y, 996488699);
+
+        let a: Modint998 = 288230376151711744.into(); // 1 << 58
+        let b: usize = 576460752303423488; // 1 << 59
+        let c: usize = 1152921504606846976; // 1 << 60
+        assert_eq!(-a - b - c, 553154679);
+    }
+
+    #[test]
+    fn test_pow() {
+        let x = Modint998::new(2);
+        let y: usize = 1000000007;
+        assert_eq!(x.pow(y), 132727571);
+
+        let a: Modint998 = MOD998.into();
+        let b: usize = 1024;
+        assert_eq!(a.pow(b), 0);
+    }
+
+    #[test]
+    fn test_inv() {
+        assert_eq!(Modint998::new(1).inv(), 1);
+        assert_eq!(Modint998::new(2).inv(), 499122177);
+        assert_eq!(Modint998::new(1000).inv(), 981274199);
+        assert_eq!(Modint998::new(998244352).inv(), 998244352);
+    }
+
+    #[test]
+    fn test_add_assign() {
+        let arr = vec![1, 2, 3];
+        let mut ans = 0;
+        for i in 0..3 {
+            ans += arr[i];
+        }
+        assert_eq!(ans, 6);
+    }
+
+    #[test]
+    fn test_mul_assign() {
+        let mut fact = vec![Modint998::new(1); 20];
+
+        // 階乗
+        for i in 1..20 {
+            let prv = fact[i - 1];
+            fact[i] *= prv * i;
+        }
+
+        assert_eq!(
+            &fact,
+            &[
+                1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600,
+                237554682, 331032489, 972509923, 586493473, 986189864, 781263551, 868586527
+            ]
+        );
+    }
 }
