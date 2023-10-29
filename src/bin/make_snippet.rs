93503b81760c87d032ddeaa8ec2b805308f9fce7
kentakom1213 <kentakom1213@gmail.com>
2023-10-29 19:09:38 +0900

    [add] 追記

--- src/bin/make_snippet.rs
+++ src/bin/make_snippet.rs
@@ -1,11 +1,26 @@
+use serde::Serialize;
+use serde_json::to_string;
 use std::{fs, path::Path};
 
-pub type FileTree = Vec<(usize, String)>;
+/// スニペットの断片
+#[derive(Serialize)]
+struct SnippetPiece {
+    prefix: String,
+    body: Vec<String>,
+    description: String,
+}
+
+/// スニペット
+#[derive(Serialize)]
+struct Snippets {
+    kyopro: Vec<SnippetPiece>,
+}
 
 const SRC: &str = "./src";
 
 fn main() {
     let files = fs::read_dir(SRC).unwrap();
+    let mut snippet = Snippets { kyopro: vec![] };
 
     // ファイルを列挙
     for entry in files {
@@ -16,19 +31,47 @@ fn main() {
             continue;
         }
 
-        let contents = read_file(&path);
-        println!("{:?}\n{}\n", &path, contents.join("\n"));
+        // ファイルの中身
+        let snippet_piece = read_file(&path);
+
+        snippet.kyopro.push(snippet_piece);
     }
+
+    println!("{}", serde_json::to_string(&snippet).unwrap());
 }
 
 /// ファイルを読み込み、不要な部分を削除する
-fn read_file(path: &Path) -> Vec<String> {
-    fs::read_to_string(path)
+fn read_file(path: &Path) -> SnippetPiece {
+    // ファイル名
+    let prefix = path.file_name().unwrap().to_str().unwrap().to_string();
+    // ファイルの中身
+    let content: Vec<String> = fs::read_to_string(path)
         .unwrap()
         .split("\n")
-        .filter(|&line| !line.is_empty() && !line.starts_with("//!")) // ファイルのdoc-commentは無視
-        .take_while(|&line| !line.starts_with("#[cfg(test)]")) // テストは無視
-        .map(|line| line.replace("    ", "\\t")) // タブ文字に置換
         .map(|line| line.to_string())
-        .collect()
+        .collect();
+
+    let mut description = String::new(); // コードの説明
+    let mut body = vec![]; // コードの中身
+
+    let mut idx = 0;
+    while idx < content.len() && content[idx].starts_with("//!") {
+        description += content[idx].as_str();
+        idx += 1;
+    }
+
+    // テスト以外の中身を追加
+    while idx < content.len() && !content[idx].starts_with("#![cfg(test)]") {
+        if content[idx] != "" {
+            let line = content[idx].to_string();
+            body.push(line);
+        }
+        idx += 1;
+    }
+
+    SnippetPiece {
+        prefix,
+        body,
+        description,
+    }
 }
