37cd7db90ffeba80b25a3963bbf8f0049beaae59
kentakom1213 <kentakom1213@gmail.com>
2024-02-15 15:26:24 +0900

    [add] get_macroを追加

---
+++ src/get_macro.rs
@@ -0,0 +1,47 @@
+//! 入力マクロ
+
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+#[macro_export]
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
