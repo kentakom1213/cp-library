5eadea5ddfd4b6f138ef290541907f14a9f7ef3f
kentakom1213 <kentakom1213@gmail.com>
2024-02-15 13:10:20 +0900

    [err] 2x2行列を実装

--- src/lazy_segment_tree.rs
+++ src/lazy_segment_tree.rs
@@ -2,9 +2,12 @@
 
 // ===== Lazy Segment Tree =====
 use core::fmt;
-use std::ops::{
-    Bound::{Excluded, Included, Unbounded},
-    RangeBounds,
+use std::{
+    fmt::Debug,
+    ops::{
+        Bound::{Excluded, Included, Unbounded},
+        RangeBounds,
+    },
 };
 
 /// 作用付きモノイド
@@ -29,14 +32,14 @@ pub trait ExtMonoid {
 
 /// 遅延評価セグメント木
 #[derive(Debug)]
-pub struct LazySegmentTree<T: ExtMonoid> {
+pub struct LazySegmentTree<M: ExtMonoid> {
     pub size: usize,
     offset: usize,
-    data: Vec<T::X>,
-    lazy: Vec<T::M>,
+    data: Vec<M::X>,
+    lazy: Vec<M::M>,
 }
 
-impl<T: ExtMonoid> LazySegmentTree<T> {
+impl<M: ExtMonoid> LazySegmentTree<M> {
     #[inline]
     fn parse_range<R: RangeBounds<usize>>(&self, range: &R) -> Option<(usize, usize)> {
         let start = match range.start_bound() {
@@ -63,29 +66,29 @@ impl<T: ExtMonoid> LazySegmentTree<T> {
         Self {
             size: n,
             offset,
-            data: vec![T::IX; offset << 1],
-            lazy: vec![T::IM; offset << 1],
+            data: vec![M::IX; offset << 1],
+            lazy: vec![M::IM; offset << 1],
         }
     }
 
     /// 遅延値を評価
     fn eval(&mut self, idx: usize, len: usize) {
-        if self.lazy[idx] == T::IM {
+        if self.lazy[idx] == M::IM {
             return;
         }
         // 葉でなければ子に伝搬
         if idx < self.offset {
-            self.lazy[idx * 2] = T::operate_m(&self.lazy[idx * 2], &self.lazy[idx]);
-            self.lazy[idx * 2 + 1] = T::operate_m(&self.lazy[idx * 2 + 1], &self.lazy[idx]);
+            self.lazy[idx * 2] = M::operate_m(&self.lazy[idx * 2], &self.lazy[idx]);
+            self.lazy[idx * 2 + 1] = M::operate_m(&self.lazy[idx * 2 + 1], &self.lazy[idx]);
         }
         // 自身を更新
-        self.data[idx] = T::apply(&self.data[idx], &T::aggregate(&self.lazy[idx], len));
-        self.lazy[idx] = T::IM;
+        self.data[idx] = M::apply(&self.data[idx], &M::aggregate(&self.lazy[idx], len));
+        self.lazy[idx] = M::IM;
     }
 
     /// 区間に`val`を作用させる
     /// - `range`: `[left, right)`
-    pub fn apply<R: RangeBounds<usize> + fmt::Debug>(&mut self, range: R, val: T::M) {
+    pub fn apply<R: RangeBounds<usize> + fmt::Debug>(&mut self, range: R, val: M::M) {
         let Some((left, right)) = self.parse_range(&range) else {
             panic!("The given range is wrong: {:?}", range);
         };
@@ -96,7 +99,7 @@ impl<T: ExtMonoid> LazySegmentTree<T> {
         &mut self,
         left: usize,
         right: usize,
-        val: T::M,
+        val: M::M,
         begin: usize,
         end: usize,
         idx: usize,
@@ -105,7 +108,7 @@ impl<T: ExtMonoid> LazySegmentTree<T> {
         self.eval(idx, end - begin);
         // 区間を内包するとき
         if left <= begin && end <= right {
-            self.lazy[idx] = T::operate_m(&self.lazy[idx], &val);
+            self.lazy[idx] = M::operate_m(&self.lazy[idx], &val);
             self.eval(idx, end - begin);
         }
         // 区間が重なるとき
@@ -116,13 +119,13 @@ impl<T: ExtMonoid> LazySegmentTree<T> {
             // 右の子を更新
             self.apply_inner(left, right, val, mid, end, idx * 2 + 1);
             // 値を更新
-            self.data[idx] = T::operate_x(&self.data[idx * 2], &self.data[idx * 2 + 1]);
+            self.data[idx] = M::operate_x(&self.data[idx * 2], &self.data[idx * 2 + 1]);
         }
     }
 
     /// 区間を取得する
     /// - `range`: `[left, right)`
-    pub fn get<R: RangeBounds<usize> + fmt::Debug>(&mut self, range: R) -> T::X {
+    pub fn get<R: RangeBounds<usize> + fmt::Debug>(&mut self, range: R) -> M::X {
         let Some((left, right)) = self.parse_range(&range) else {
             panic!("The given range is wrong: {:?}", range);
         };
@@ -136,12 +139,12 @@ impl<T: ExtMonoid> LazySegmentTree<T> {
         begin: usize,
         end: usize,
         idx: usize,
-    ) -> T::X {
+    ) -> M::X {
         // 遅延値を評価
         self.eval(idx, end - begin);
         // 区間を含まない
         if end <= left || right <= begin {
-            T::IX
+            M::IX
         }
         // 区間を包含する
         else if left <= begin && end <= right {
@@ -152,26 +155,50 @@ impl<T: ExtMonoid> LazySegmentTree<T> {
             let mid = (begin + end) / 2;
             let l_val = self.get_inner(left, right, begin, mid, idx * 2);
             let r_val = self.get_inner(left, right, mid, end, idx * 2 + 1);
-            T::operate_x(&l_val, &r_val)
+            M::operate_x(&l_val, &r_val)
         }
     }
 }
 
-impl<T: ExtMonoid> From<&Vec<T::X>> for LazySegmentTree<T> {
-    fn from(src: &Vec<T::X>) -> Self {
+impl<M: ExtMonoid> From<&Vec<M::X>> for LazySegmentTree<M> {
+    fn from(src: &Vec<M::X>) -> Self {
         let mut seg = Self::new(src.len());
         for (i, v) in src.iter().enumerate() {
             seg.data[seg.offset + i] = v.clone();
         }
         for i in (0..seg.offset).rev() {
             let lch = i << 1;
-            seg.data[i] = T::operate_x(&seg.data[lch], &seg.data[lch + 1]);
+            seg.data[i] = M::operate_x(&seg.data[lch], &seg.data[lch + 1]);
         }
         seg
     }
 }
 
+impl<M> LazySegmentTree<M>
+where
+    M: ExtMonoid,
+    M::M: Debug,
+    M::X: Debug,
+{
+    pub fn show(&mut self) -> String {
+        let mut res = format!("LazySegmentTree {{ [");
+        for i in 0..self.size {
+            if i + 1 < self.size {
+                res += &format!("{:?}, ", self.get(i..=i));
+            } else {
+                res += &format!("{:?}", self.get(i..=i));
+            }
+        }
+        res += "] }}";
+        res
+    }
+}
+
 pub mod Alg {
+    use std::ops::{Add, Mul};
+
+    use num_traits::{One, Zero};
+
     use super::ExtMonoid;
 
     /// ## RSQandRAQ
@@ -271,10 +298,73 @@ pub mod Alg {
             x.map(|x| x * p as isize)
         }
     }
+
+    /// ## 1次元Affine変換
+    /// - 区間を`ax + b`で更新（Affine変換）
+    /// - 区間和を取得
+    #[derive(Debug)]
+    pub struct Affine1D;
+    impl ExtMonoid for Affine1D {
+        type X = Mat2x2<isize>;
+        type M = Mat2x2<isize>;
+        const IX: Self::X = Mat2x2([[0, 0], [0, 1]]);
+        const IM: Self::M = Mat2x2([[1, 0], [0, 1]]);
+        fn operate_x(x: &Self::X, y: &Self::X) -> Self::X {
+            y.add(x)
+        }
+        fn operate_m(x: &Self::M, y: &Self::M) -> Self::M {
+            y.dot(x)
+        }
+        fn apply(x: &Self::X, y: &Self::M) -> Self::X {
+            y.dot(x)
+        }
+        fn aggregate(x: &Self::M, _: usize) -> Self::M {
+            x.clone()
+        }
+    }
+
+    /// 2x2行列を表す構造体
+    #[derive(Debug, PartialEq, Clone)]
+    pub struct Mat2x2<T>([[T; 2]; 2]);
+
+    impl<T> Mat2x2<T>
+    where
+        T: Copy + Add<Output = T> + Mul<Output = T> + Zero + One,
+    {
+        /// 和を求める
+        fn add(&self, rhs: &Self) -> Self {
+            let &Mat2x2([[x00, x01], [x10, x11]]) = self;
+            let &Mat2x2([[y00, y01], [y10, y11]]) = rhs;
+            Mat2x2([[x00 + y00, x01 + y01], [x10 + y10, x11 + y11]])
+        }
+
+        /// 積を求める
+        fn dot(&self, rhs: &Self) -> Self {
+            let &Mat2x2([[x00, x01], [x10, x11]]) = self;
+            let &Mat2x2([[y00, y01], [y10, y11]]) = rhs;
+            Mat2x2([
+                [x00 * y00 + x01 * y10, x00 * y01 + x01 * y11],
+                [x10 * y00 + x11 * y10, x10 * y01 + x11 * y11],
+            ])
+        }
+
+        /// `ax + b`を表す行列を生成する
+        pub fn create(a: T, b: T) -> Self {
+            Mat2x2([[a, b], [T::zero(), T::one()]])
+        }
+
+        /// 現在の行列が表している値を取得する
+        pub fn eval(&self) -> T {
+            let &Mat2x2([[a, b], _]) = self;
+            a + b
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use test::Alg::Mat2x2;
+
     use super::*;
 
     #[test]
@@ -324,6 +414,7 @@ mod test {
     fn test_RMQ_and_RUQ_hand() {
         const INF: isize = (1 << 31) - 1;
         let mut seg = LazySegmentTree::<Alg::RMQandRUQ>::new(10);
+        eprintln!("{}", seg.show());
         // [INF, INF, INF, INF, INF, INF, INF, INF, INF, INF]
 
         assert_eq!(seg.get(..), INF);
@@ -332,6 +423,7 @@ mod test {
         assert_eq!(seg.get(3..8), INF);
 
         seg.apply(0..4, 2);
+        eprintln!("{}", seg.show());
         // [2, 2, 2, 2, INF, INF, INF, INF, INF, INF]
 
         assert_eq!(seg.get(..), 2);
@@ -340,6 +432,7 @@ mod test {
         assert_eq!(seg.get(3..8), 2);
 
         seg.apply(4.., 5);
+        eprintln!("{}", seg.show());
         // [2, 2, 2, 2, 5, 5, 5, 5, 5, 5]
 
         assert_eq!(seg.get(..), 2);
@@ -348,6 +441,7 @@ mod test {
         assert_eq!(seg.get(3..8), 2);
 
         seg.apply(2..=5, -3);
+        eprintln!("{}", seg.show());
         // [2, 2, -3, -3, -3, -3, 5, 5, 5, 5]
 
         assert_eq!(seg.get(..), -3);
@@ -356,6 +450,7 @@ mod test {
         assert_eq!(seg.get(3..8), -3);
 
         seg.apply(8..10, -10);
+        eprintln!("{}", seg.show());
         // [2, 2, -3, -3, -3, -3, 5, 5, -10, -10]
 
         assert_eq!(seg.get(..), -10);
@@ -368,14 +463,17 @@ mod test {
     #[test]
     fn test_RSQ_and_RUQ() {
         let mut seg = LazySegmentTree::<Alg::RSQandRUQ>::new(6);
+        eprintln!("{}", seg.show());
 
         seg.apply(1..=3, Some(1));
         seg.apply(2..=4, Some(-2));
+        eprintln!("{}", seg.show());
 
         assert_eq!(seg.get(..=5), -5);
         assert_eq!(seg.get(..=1), 1);
 
         seg.apply(3..=5, Some(3));
+        eprintln!("{}", seg.show());
 
         assert_eq!(seg.get(3..=4), 6);
         assert_eq!(seg.get(..=5), 8);
@@ -442,5 +540,46 @@ mod test {
         let mut seg = LazySegmentTree::<Alg::RMQandRAQ>::from(&vec![0, 1, 2, 3, 4, 5]);
 
         seg.apply(..7, 0);
+    }
+
+    #[test]
+    fn test_range_affine() {
+        // 配列arrの[l,r)をa倍し，bを加算
+        let apply = |arr: &mut Vec<isize>, (l, r): (usize, usize), (a, b): (isize, isize)| {
+            for i in l..r {
+                arr[i] *= a;
+                arr[i] += b;
+            }
+        };
+
+        // 配列arrの[l,r)の総和を取得
+        let get =
+            |arr: &Vec<isize>, (l, r): (usize, usize)| -> isize { arr[l..r].iter().sum::<isize>() };
+
+        let N = 6;
+        let mut arr = vec![0; N];
+        let mut seg = LazySegmentTree::<Alg::Affine1D>::new(N);
+        eprintln!("{:?}", seg);
+        eprintln!("{}", seg.show());
+
+        for l in 0..N {
+            for r in l..=N {
+                eprintln!("[{}, {}) -> {}", l, r, get(&arr, (l, r)));
+                assert_eq!(get(&arr, (l, r)), seg.get(l..r).eval());
+            }
+        }
+
+        // [0, 3) に `x + 2` を作用
+        apply(&mut arr, (0, 3), (1, 2));
+        seg.apply(..3, Mat2x2::create(1, 2));
+        eprintln!("{:?}", seg);
+        eprintln!("{}", seg.show());
+
+        for l in 0..N {
+            for r in l..=N {
+                eprintln!("[{}, {}) -> {}", l, r, get(&arr, (l, r)));
+                assert_eq!(get(&arr, (l, r)), seg.get(l..r).eval());
+            }
+        }
     }
 }
