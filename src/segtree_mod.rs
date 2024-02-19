37deb8554c3ae6a7b73936bfa50d347731fe89c0
kentakom1213 <kentakom1213@gmail.com>
2024-02-19 09:56:49 +0900

    [add] アフィン変換を追加

--- src/segtree_mod.rs
+++ src/segtree_mod.rs
@@ -20,3 +20,15 @@ impl<const MOD: usize> Monoid for ModMul<MOD> {
         *left * *right
     }
 }
+
+/// アフィン変換
+pub struct Affine<const MOD: usize>;
+impl<const MOD: usize> Monoid for Affine<MOD> {
+    type Val = (Modint<MOD>, Modint<MOD>);
+    const E: Self::Val = (Modint::<MOD>(1), Modint::<MOD>(0));
+    fn op(left: &Self::Val, right: &Self::Val) -> Self::Val {
+        let &(a1, b1) = left;
+        let &(a2, b2) = right;
+        (a2 * a1, a2 * b1 + b2)
+    }
+}
