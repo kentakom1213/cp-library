941cb58ef2e30c8f5361cd4d223bc2602a32ab60
kentakom1213 <kentakom1213@gmail.com>
2024-06-19 00:35:22 +0900

    [add] synクレートによるソースコード解析

--- expander/src/utils.rs
+++ expander/src/utils.rs
@@ -1,66 +1,71 @@
-/// DFSにより，再帰的に依存関係を列挙する
-pub fn enum_dependancies_recursive() {}
+macro_rules! debug {
+    ( $($val:expr),* $(,)* ) => {{
+        #[cfg(debug_assertions)]
+        eprintln!( concat!($(stringify!($val), " = {:?}, "),*), $($val),* );
+    }};
+}
 
-/// ソースコードをトークン列に分解する
-fn tokenize(source: &str) -> Vec<String> {
-    let mut tokens = vec![];
+use std::{collections::HashSet, fs};
+use syn::{visit::Visit, File, Item, UseTree};
 
-    for t in source.split_ascii_whitespace() {
-        let mut last = 0;
-        for (i, c) in t.chars().enumerate() {
-            match c {
-                ',' | ';' | '{' | '}' | '(' | ')' => {
-                    tokens.push(t[last..i].to_string());
-                    tokens.push(t[i..=i].to_string());
-                    last = i + 1;
-                }
-                ':' => {
-                    if i + 1 < t.len() && &t[i..=i + 1] == "::" {
-                        tokens.push(t[last..i].to_string());
-                        tokens.push(t[i..=i + 1].to_string());
-                        last = i + 2;
-                    } else if last <= i {
-                        tokens.push(t[last..i].to_string());
-                        tokens.push(t[i..=i].to_string());
-                        last = i + 1;
-                    }
-                }
-                _ => (),
+struct UseVisitor {
+    uses: Vec<Vec<String>>,
+}
+
+impl<'ast> Visit<'ast> for UseVisitor {
+    fn visit_item_use(&mut self, item_use: &'ast syn::ItemUse) {
+        let use_tree = &item_use.tree;
+        let mut paths = Vec::new();
+        use_tree_to_vec(use_tree, &mut paths, Vec::new());
+        self.uses.extend(paths);
+        // Continue visiting the rest of the file
+        syn::visit::visit_item_use(self, item_use);
+    }
+}
+
+fn use_tree_to_vec(tree: &UseTree, paths: &mut Vec<Vec<String>>, mut current_path: Vec<String>) {
+    match tree {
+        UseTree::Path(path) => {
+            current_path.push(path.ident.to_string());
+            use_tree_to_vec(&*path.tree, paths, current_path);
+        }
+        UseTree::Name(name) => {
+            current_path.push(name.ident.to_string());
+            paths.push(current_path);
+        }
+        UseTree::Rename(rename) => {
+            current_path.push(rename.ident.to_string());
+            paths.push(current_path);
+        }
+        UseTree::Glob(_) => {
+            current_path.push("*".to_string());
+            paths.push(current_path);
+        }
+        UseTree::Group(group) => {
+            for item in &group.items {
+                use_tree_to_vec(item, paths, current_path.clone());
             }
         }
-        if last < t.len() {
-            tokens.push(t[last..].to_string());
-        }
     }
-
-    tokens
 }
 
-#[cfg(test)]
-mod test_utils {
-    use crate::utils::tokenize;
-
-    #[test]
-    fn test_parse_dependancies() {
-        let source1 = r#"use crate::{a, b::{c, d}, e};
-use std::collections::BTreeMap;
-
-fn func<T>(t: T)
-where
-    T: Ord,
-{
-    todo!()
-}
+#[test]
+fn test() {
+    let content = r#"
+use cp_library_rs::{
+    modint::{M998, M109},
+    segment_tree::SegmentTree,
+};
+use cp_library_rs::monoid::{Monoid, examples::*};
 
 fn main() {
-    println!("Hello, World");
-    return 0;
-}"#;
 
-        println!("{}", source1);
+}
+"#;
+    let syntax_tree: File = syn::parse_file(&content).expect("Unable to parse file");
 
-        let tokens = tokenize(source1);
+    let mut visitor = UseVisitor { uses: vec![] };
+    visitor.visit_file(&syntax_tree);
 
-        println!("{:?}", tokens);
-    }
+    debug!(visitor.uses);
 }
