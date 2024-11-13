f56c43041154604bce14f0587b2f06ad0208bfcd
kentakom1213 <kentakom1213@gmail.com>
2024-11-14 00:43:02 +0900

    [add] セグ木を追加

---
+++ cp-library-py/tests/test_segment_tree.py
@@ -0,0 +1,29 @@
+import unittest
+
+from src.cp_library_py.segment_tree import SegmentTree
+
+
+class TestSegmentTree(unittest.TestCase):
+    def test_sum(self):
+        seg = SegmentTree.from_array(
+            [1, 2, 3, 4, 5], lambda: 0, lambda a, b: a + b)
+
+        self.assertEqual(seg.get_range(0, 5), 15)
+        self.assertEqual(seg.get_range(1, 4), 9)
+        self.assertEqual(seg.get_range(2, 3), 3)
+        seg.update(2, 10)
+        self.assertEqual(seg.get_range(0, 5), 22)
+        self.assertEqual(seg.get_range(1, 4), 16)
+        self.assertEqual(seg.get_range(2, 3), 10)
+
+    def test_min(self):
+        seg = SegmentTree.from_array(
+            [1, 2, 3, 4, 5], lambda: 1e20, min)
+
+        self.assertEqual(seg.get_range(0, 5), 1)
+        self.assertEqual(seg.get_range(1, 4), 2)
+        self.assertEqual(seg.get_range(2, 3), 3)
+        seg.update(1, -1)
+        self.assertEqual(seg.get_range(0, 5), -1)
+        self.assertEqual(seg.get_range(1, 4), -1)
+        self.assertEqual(seg.get_range(2, 3), 3)
