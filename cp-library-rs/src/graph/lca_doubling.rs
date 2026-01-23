dfb200944572d9f71c8d0b58ac1fa71138ab376d
kentakom1213 <kentakom1213@gmail.com>
2026-01-23 16:34:52 +0900

    add: LCA の機能追加

--- cp-library-rs/src/graph/lca_doubling.rs
+++ cp-library-rs/src/graph/lca_doubling.rs
@@ -1,11 +1,14 @@
 //! ダブリングにより、最小共通祖先を求める
 
-const INF: usize = 1001001001001001001;
+use crate::utils::consts::INF;
+
 type Graph = Vec<Vec<usize>>;
 
-/// # LCA
-/// 最小共通祖先を求めるクエリに答える
+/// LCA
+/// - 最小共通祖先を求めるクエリに答える
 pub struct LCA {
+    root: usize,
+    logn: usize,
     double: Vec<Vec<usize>>,
     depth: Vec<usize>,
 }
@@ -13,28 +16,21 @@ pub struct LCA {
 impl LCA {
     /// `root`を根に持つ木`tree`で、初期化を行う
     pub fn new(tree: &Graph, root: usize) -> Self {
-        let V = tree.len(); // グラフの頂点数
-        let logV = {
-            // log_2(グラフの頂点数)
-            let mut logv = 0;
-            while (V >> logv) > 0 {
-                logv += 1;
-            }
-            logv
-        };
-        let mut double = vec![vec![0; V]; logV]; // ダブリング配列
-        let mut depth = vec![INF; V]; // 頂点の根からの距離
+        let n = tree.len(); // グラフの頂点数
+        let logn = n.next_power_of_two().trailing_zeros() as usize;
+        let mut double = vec![vec![0; n]; logn]; // ダブリング配列
+        let mut depth = vec![INF; n]; // 頂点の根からの距離
         depth[0] = 0;
         Self::dfs(root, &mut double[0], &mut depth, tree);
 
         // ダブリング
-        for i in 1..logV {
-            for j in 0..V {
+        for i in 1..logn {
+            for j in 0..n {
                 double[i][j] = double[i - 1][double[i - 1][j]];
             }
         }
 
-        Self { double, depth }
+        Self { root, logn, double, depth }
     }
 
     fn dfs(u: usize, par: &mut Vec<usize>, depth: &mut Vec<usize>, tree: &Graph) {
@@ -48,15 +44,25 @@ impl LCA {
         }
     }
 
-    /// 頂点`u`,`v`の最小共通祖先を求める
-    pub fn get_lca(&self, mut u: usize, mut v: usize) -> usize {
+    /// 頂点 `v` の深さを求める
+    pub fn depth(&self, v: usize) -> usize {
+        self.depth[v]
+    }
+
+    /// 頂点 `v` の親を求める
+    pub fn parent(&self, v: usize) -> Option<usize> {
+        (v != self.root).then_some(self.double[0][v])
+    }
+
+    /// 頂点 `u`,`v` の最小共通祖先を求める
+    pub fn lca(&self, mut u: usize, mut v: usize) -> usize {
         // 常にuを深くする
         if self.depth[u] < self.depth[v] {
-            std::mem::swap(&mut u, &mut v);
+            (u, v) = (v, u);
         }
 
         // LCAまでの距離を同じにする
-        for k in 0..self.double.len() {
+        for k in 0..self.logn {
             if ((self.depth[u] - self.depth[v]) >> k) & 1 == 1 {
                 u = self.double[k][u];
             }
@@ -67,7 +73,7 @@ impl LCA {
         }
 
         // 二分探索
-        for k in (0..self.double.len()).rev() {
+        for k in (0..self.logn).rev() {
             if self.double[k][u] != self.double[k][v] {
                 u = self.double[k][u];
                 v = self.double[k][v];
@@ -77,9 +83,36 @@ impl LCA {
         self.double[0][u]
     }
 
-    /// 頂点`u`,`v`の距離を求める
+    /// 頂点 `u`,`v` の距離を求める
     pub fn dist(&self, u: usize, v: usize) -> usize {
-        let o = self.get_lca(u, v);
+        let o = self.lca(u, v);
         (self.depth[u] - self.depth[o]) + (self.depth[v] - self.depth[o])
+    }
+
+    /// 頂点 `v` の k 個上の祖先を求める
+    pub fn kth_ancestor(&self, mut v: usize, k: usize) -> Option<usize> {
+        if k > self.depth(v) {
+            return None;
+        }
+        for i in 0..self.logn {
+            if (k >> i) & 1 == 1 {
+                v = self.double[i][v];
+            }
+        }
+        Some(v)
+    }
+
+    /// 頂点 `u` から `v` へ向かうパス上の `k` 個目の頂点を求める
+    pub fn kth_on_path(&self, u: usize, v: usize, k: usize) -> Option<usize> {
+        let o = self.lca(u, v);
+        let dist_u_o = self.depth[u] - self.depth[o];
+        if k <= dist_u_o {
+            self.kth_ancestor(u, k)
+        } else {
+            let dist_v_o = self.depth[v] - self.depth[o];
+            (dist_v_o + dist_u_o)
+                .checked_sub(k)
+                .and_then(|l| self.kth_ancestor(v, l))
+        }
     }
 }
