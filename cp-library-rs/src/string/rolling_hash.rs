599f6cd99d301836ede0f6ec8f88faf96ecc72d6
kentakom1213 <kentakom1213@gmail.com>
2026-02-13 16:38:27 +0900

    add: ローリングハッシュの修正

--- cp-library-rs/src/string/rolling_hash.rs
+++ cp-library-rs/src/string/rolling_hash.rs
@@ -1,5 +1,10 @@
 //! ローリングハッシュ
 
+use std::{
+    fmt::Debug,
+    ops::{Bound, RangeBounds},
+};
+
 use crate::number_theory::modint_for_rollinghash::modint::Modint;
 
 use num_traits::{One, Zero};
@@ -47,24 +52,23 @@ impl RollingHash {
 
     /// `l..r`のハッシュを取得
     /// - 時間計算量: $`O(1)`$
-    pub fn get(&self, l: usize, r: usize) -> Modint {
-        self.hash[r] - self.hash[l] * self.power[r - l]
+    pub fn hash<'a, R: RangeBounds<usize> + Debug>(&'a self, range: R) -> HashVal<'a> {
+        let (l, r) = self.parse_range(&range);
+        HashVal {
+            rolling_hash: self,
+            length: r - l,
+            hash: self.hash[r] - self.hash[l] * self.power[r - l],
+        }
     }
 
-    /// `0..size`のハッシュを取得
-    /// - 時間計算量: $`O(1)`$
-    pub fn full(&self) -> Modint {
-        self.hash[self.size]
-    }
-
-    /// a,bからの最長共通接頭辞の長さを調べる
+    /// `S[a..]`, `S[b..]` の最長共通接頭辞の長さを調べる
     /// - 時間計算量: $`O(\log N)`$
-    pub fn getLCP(&self, a: usize, b: usize) -> usize {
+    pub fn get_LCP(&self, a: usize, b: usize) -> usize {
         let len = self.size.saturating_sub(a.max(b));
         let (mut lo, mut hi) = (0, len + 1);
         while hi - lo > 1 {
             let mid = (lo + hi) / 2;
-            if self.get(a, a + mid) == self.get(b, b + mid) {
+            if self.hash(a..a + mid) == self.hash(b..b + mid) {
                 lo = mid;
             } else {
                 hi = mid;
@@ -73,17 +77,82 @@ impl RollingHash {
         lo
     }
 
-    /// ハッシュ同士を連結
-    /// - 時間計算量: $`O(1)`$
-    pub fn concat(&self, h1: Modint, h2: Modint, h2_len: usize) -> Modint {
-        h1 * self.power[h2_len] + h2
+    pub fn len(&self) -> usize {
+        self.size
     }
 
-    /// `A`を`0`とするascii文字(`A~Za~z`)のインデックスを返す
+    // ========== internal ==========
+    #[inline]
+    fn parse_range<R: RangeBounds<usize> + Debug>(&self, range: &R) -> (usize, usize) {
+        let start = match range.start_bound() {
+            Bound::Unbounded => 0,
+            Bound::Excluded(&v) => v + 1,
+            Bound::Included(&v) => v,
+        };
+        let end = match range.end_bound() {
+            Bound::Unbounded => self.size,
+            Bound::Excluded(&v) => v,
+            Bound::Included(&v) => v + 1,
+        };
+        if start <= end && end <= self.size {
+            (start, end)
+        } else {
+            panic!(
+                "Index out of bounds: the len is {} but the range is {:?}",
+                self.size, range
+            );
+        }
+    }
+
+    /// 文字 c の ASCII コードを返す
     #[inline]
     fn ord(c: char) -> usize {
-        let a = 'A' as u32;
-        let c = c as u32;
-        (c - a) as usize
+        c as usize
+    }
+}
+
+#[derive(Clone)]
+pub struct HashVal<'a> {
+    rolling_hash: &'a RollingHash,
+    length: usize,
+    hash: Modint,
+}
+
+impl<'a> HashVal<'a> {
+    /// ハッシュの値を返す
+    /// - 時間計算量: $`O(1)`$
+    pub fn val(&self) -> Modint {
+        self.hash
+    }
+
+    /// ハッシュ同士を連結
+    /// - 時間計算量: $`O(1)`$
+    pub fn chain(&self, other: &HashVal<'a>) -> HashVal<'a> {
+        Self {
+            rolling_hash: self.rolling_hash,
+            length: self.len() + other.len(),
+            hash: self.val() * self.rolling_hash.power[other.len()] + other.val(),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
+    }
+}
+
+impl PartialEq for HashVal<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.len() == other.len() && self.val() == other.val()
+    }
+}
+
+impl<'a> Eq for HashVal<'a> {}
+
+impl Debug for HashVal<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_map()
+            .entry(&"length", &self.length)
+            .entry(&"hash", &self.hash)
+            .finish()
     }
 }
