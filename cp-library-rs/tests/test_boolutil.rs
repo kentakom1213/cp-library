1a263d25b4734165b11dd76da9a91c2e4536851b
kentakom1213 <kentakom1213@gmail.com>
2026-02-14 23:51:57 +0900

    add: 機能追加など

--- cp-library-rs/tests/test_boolutil.rs
+++ cp-library-rs/tests/test_boolutil.rs
@@ -1,10 +1,16 @@
 #![allow(non_snake_case)]
 
-use cp_library_rs::utils::yesno::YesNo;
+use cp_library_rs::utils::boolutil::BoolUtil;
 
 #[test]
 fn yesno() {
     assert_eq!(true.yesno(), "Yes");
     assert_eq!(false.yesno(), "No");
     assert_eq!((1 + 1 == 2).yesno(), "Yes");
+}
+
+#[test]
+fn endl() {
+    assert_eq!(false.endl(), " ");
+    assert_eq!(true.endl(), "\n");
 }
